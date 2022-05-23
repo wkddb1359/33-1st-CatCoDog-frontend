f4631314f69d0df767b60632424c52fcc333bdde
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-23 17:54:13 +0900

    Add: 수정완료

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/common.scss';
 import './styles/reset.scss';
+import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(<Router />);
