511d6604fe36ae59d266d881b73d76c05dfbe4e5
Seung Ju <97432901+wkddb1359@users.noreply.github.com>
2022-05-24 22:16:22 +0900

    Add : goodsDetail 페이지 구현 중

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Detail.scss';
+import Nav from '../../components/nav/Nav';
+import Footer from '../../components/footer/Footer';
 
 function Detail() {
   return (
     <>
-      <div className="nav">nav</div>
+      <Nav />
       <main className="seongjuMain">
         <div className="inside">
           <div className="goodsWrapper">
-            <div className="goodsImg">
+            <div className="goodsImgContainer">
+              <img
+                src="./images/123.png"
+                className="goodsImg"
+                width={610}
+                height={610}
+              />
               <div className="goodsSmallImg">
                 <ul>
                   <li></li>
@@ -21,10 +29,32 @@ function Detail() {
                 </ul>
               </div>
             </div>
-            <div className="goodsForm"></div>
+            <div className="goodsFormWrapper">
+              <div className="goodsFormContainer">
+                <header className="goodsFormHeader">
+                  <div className="goodsFormHeaderTop">
+                    <span className="goodsFormHeaderTitle">
+                      바른트릿 9종 골라담기
+                    </span>
+                    <span className="goodsFormHeaderSale">SALE</span>
+                    <span className="goodsFormHeaderBest">BEST</span>
+                  </div>
+                  <div className="goodsFormHeaderPrice">
+                    <div className="originalPrice">7,900원</div>
+                    <span className="saleRate">13%</span>
+                    <span className="salePrice">6,900원</span>
+                  </div>
+                </header>
+                <div className="goodsFormItemSummary"></div>
+                <div className="goodsFormItemDetail"></div>
+                <div className="goodsFormItemFlavor"></div>
+                <div className="goodsFormButton"></div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
+      <Footer />
     </>
   );
 }
