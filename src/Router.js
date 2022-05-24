8f8147f3cc2551d59f78436b6fc2d88dc0d01bd8
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-24 13:33:06 +0900

    Add: 공통컴포넌트(Nav, Footer)추가, Router 작성

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
+import Detail from './pages/Detail/Detail';
+import List from './pages/List/List';
+import Main from './pages/Main/Main';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>{/*    <Route path="/" element={< />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </BrowserRouter>
   );
 };
---
+++ src/pages/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../../components/Footer/Footer';
+import Nav from '../../components/Nav/Nav';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <Nav />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
