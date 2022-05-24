9be68efac0e40a52d0983a9caa0bf0413061f336
Seung Ju <97432901+wkddb1359@users.noreply.github.com>
2022-05-24 15:22:15 +0900

    Add : 레이아웃 구성

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Detail from './pages/detail/Detail';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>{/*    <Route path="/" element={< />} /> */}</Routes>
+      <Routes>
+        <Route path="/detail" element={<Detail />} />{' '}
+      </Routes>
     </BrowserRouter>
   );
 };
---
+++ src/pages/detail/Detail.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Detail.scss';
+
+function Detail() {
+  return (
+    <>
+      <div className="nav">nav</div>
+      <main className="seongjuMain">
+        <div className="inside">
+          <div className="goodsWrapper">
+            <div className="goodsImg">
+              <div className="goodsSmallImg">
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+            <div className="goodsForm"></div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Detail;
