7d07d58c85f669f6a00a91aed372d70cec618b55
Haim Chi <ihaimichi@gmail.com>
2022-05-26 17:51:44 +0900

    로그인 페이지, 백엔드 통신完

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import './Login.scss';
@@ -6,37 +6,73 @@ import './Login.scss';
 const Login = () => {
   const [inputValue, setInputValue] = useState({
     email: '',
-    pw: '',
+    password: '',
   });
 
-  const { email, pw } = inputValue;
+  const { email, password } = inputValue;
 
   const handleInput = e => {
     const { name, value } = e.target;
     setInputValue({ ...inputValue, [name]: value });
   };
 
-  const passwordCodition =
+  const passwordCondition =
     /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
 
-  // const valid = passwordCodition.test(inputValue.pw);
+  const valid = passwordCondition.test(inputValue.password);
 
   // const specialLetter = pw.search(/[`s~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
   // const num = pw.search(/[0-9]/g);
   // const engLetter = pw.search(/[A-z a-z]/gi);
+
   const isValidLogin =
     email.includes('@') &&
     email.includes('.') &&
-    passwordCodition.test(inputValue.pw);
+    passwordCondition.test(inputValue.password);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const goToMain = () => {
-    navigate('/');
-  };
+  // const goToMain = e => {
+  //   e.preventDefault();
+  //   navigate('/');
+  // };
+
+  // useEffect(() => {
+  //   fetch('http://10.58.2.1:8000/users/signin', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       email: 'inputValue.email',
+  //       password: 'inputValue.password',
+  //     }),
+  //   }) //덩어리 제이슨을 받아옴
+  //     .then(res => res.json()) //덩어리 제이슨을 객체 현태로 변환
+  //     .then(data => {
+  //       goToMain(data);
+  //     }); //변환된 제이슨 덩어리를 setCommentList에 저장해서 관리
+  // }, []);
 
   const goToSignUp = () => {
-    navigate('/haimsignup');
+    navigate('/signup');
+  };
+  const navigate = useNavigate();
+
+  const goToMain = e => {
+    e.preventDefault();
+    fetch('http://10.58.2.1:8000/users/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: inputValue.email,
+        password: inputValue.password,
+      }),
+    })
+      .then(res => res.json())
+      .then(result => {
+        if (result.ACCESS_TOKEN) {
+          console.log(result.ACCESS_TOKEN);
+          localStorage.setItem('ACCESS_TOKEN', result.ACCESS_TOKEN);
+        }
+      });
+    navigate('/');
   };
 
   return (
@@ -90,8 +126,8 @@ const Login = () => {
                 className="pwInputBox"
                 type="password"
                 placeholder="비밀번호"
-                value={pw}
-                name="pw"
+                value={password}
+                name="password"
                 onChange={handleInput}
               />
             </div>
