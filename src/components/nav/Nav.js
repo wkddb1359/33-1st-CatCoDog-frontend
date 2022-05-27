8e167dfd1adf30ace04b8c67ef777c526c5282ea
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-27 18:25:14 +0900

    Add: 메인에서 Nav삭제

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,31 +1,7 @@
 import React from 'react';
-import NavBottom from '../NavBottom/NavBottom';
-import './Nav.scss';
 
 const Nav = () => {
-  return (
-    <div className="navWrap">
-      <div className="nav">
-        <div className="navTop">
-          <div className="navTopBox">
-            <a>
-              <strong>
-                <span className="navText">간식 1팩 100원부터!</span>
-              </strong>
-            </a>
-          </div>
-        </div>
-        <div className="navMiddle">
-          <div className="middleLeft"></div>
-          <div className="middleCenter"></div>
-          <div className="middelRight"></div>
-        </div>
-        <div className="navBottom">
-          <NavBottom />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Nav</div>;
 };
 
 export default Nav;
