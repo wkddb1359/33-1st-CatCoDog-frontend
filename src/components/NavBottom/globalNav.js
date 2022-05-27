adcf248f7ef3b29a4ecb270c497990ae68943469
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-27 19:30:12 +0900

    ADD : nav 레이아웃 완료

---
+++ src/components/NavBottom/globalNav.js
@@ -0,0 +1,31 @@
+const GLOBAL_NAV = [
+  { id: 1, name: '베스트', list: [''] },
+  {
+    id: 2,
+    name: '고양이',
+    list: [
+      { id: 1, listname: '고양이 전체상품' },
+      { id: 2, listname: '통살' },
+      { id: 3, listname: '동결건조' },
+      { id: 4, listname: '유산균/영양제' },
+      { id: 5, listname: '습식/파우치' },
+    ],
+  },
+  {
+    id: 3,
+    name: '강아지',
+    list: [
+      { id: 1, listname: '강아지 전체상품' },
+      { id: 2, listname: '통살' },
+      { id: 3, listname: '동결건조' },
+      { id: 4, listname: '유산균/영양제' },
+      { id: 5, listname: '저키/트릿' },
+      { id: 6, listname: '습식/파우치' },
+      { id: 7, listname: '껌' },
+      { id: 8, listname: '수제간식' },
+    ],
+  },
+  { id: 4, name: '용품', list: [''] },
+  { id: 5, name: '대용량존', list: [''] },
+];
+export default GLOBAL_NAV;
