73b02f1c8268fedfbaaac068170d9b72bbb3e447
Seung Ju <97432901+wkddb1359@users.noreply.github.com>
2022-05-30 14:23:45 +0900

    Add : 제품 미니 사진 기능 추가

--- src/pages/Detail/DetailGoods/DetailGoods.js
+++ src/pages/Detail/DetailGoods/DetailGoods.js
@@ -2,99 +2,107 @@ import React, { useEffect, useState } from 'react';
 import './DetailGoods.scss';
 
 function DetailGoods() {
-  const [products, setProducts] = useState([]);
+  const [goodsImg, setGoodsImg] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/data/goodsdata.json')
+    fetch('/data/goodsdata.json')
       .then(res => res.json())
-      .then(data => setProducts(data));
+      .then(data => setGoodsImg(data));
   }, []);
 
-  return (
-    <div className="detailGoods">
-      <div className="goodsImgContainer">
-        <img
-          src="https://velog.velcdn.com/images/cadyky95/post/26f6e5de-884d-456c-8320-17308c161728/image.png"
-          className="goodsImg"
-          width={610}
-          height={610}
-          alt="제품사진"
-        />
+  const [mainImgURL, setMainImgURL] = useState('');
 
-        <div className="goodsSmallImg">
-          <ul>
-            {products.map(productsArry => {
-              return (
-                <li className="smallImgList" key={productsArry.id}>
-                  <a href="#top">
-                    <img
-                      src={productsArry.goodsImg}
-                      width={60}
-                      height={60}
-                      alt="제품미니사진"
-                    />
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className="goodsFormWrapper">
-        <div className="goodsFormContainer">
-          <header className="goodsFormHeader">
-            <div className="goodsFormHeaderTop">
-              <span className="goodsFormHeaderTitle">
-                바른트릿 9종 골라담기
-              </span>
-              <span className="goodsFormHeaderSale">SALE</span>
-              <span className="goodsFormHeaderBest">BEST</span>
+  useEffect(() => {
+    setMainImgURL(
+      Object.keys(goodsImg).length !== 0 ? goodsImg.result.goodsURL[0] : ''
+    );
+  }, [goodsImg]);
+
+  return (
+    <div>
+      {mainImgURL && (
+        <div className="detailGoods">
+          <div className="goodsImgContainer">
+            <img
+              src={mainImgURL}
+              className="goodsImg"
+              width={610}
+              height={610}
+              alt="제품사진"
+            />
+            <div className="goodsSmallImg">
+              {goodsImg.result.goodsURL.map((a, i) => {
+                return (
+                  <div
+                    className="smallImgList"
+                    key={i}
+                    onClick={() => {
+                      setMainImgURL(a);
+                    }}
+                  >
+                    <img src={a} width={60} height={60} alt="제품미니사진" />
+                  </div>
+                );
+              })}
             </div>
-            <div className="goodsFormHeaderPrice">
-              <div className="originalPrice">7,900원</div>
-              <span className="saleRate">13%</span>
-              <span className="salePrice">6,900원</span>
-              <i className="fa-solid fa-share-nodes" />
+          </div>
+          <div className="goodsFormWrapper">
+            <div className="goodsFormContainer">
+              <header className="goodsFormHeader">
+                <div className="goodsFormHeaderTop">
+                  <span className="goodsFormHeaderTitle">
+                    바른트릿 9종 골라담기
+                  </span>
+                  <span className="goodsFormHeaderSale">SALE</span>
+                  <span className="goodsFormHeaderBest">BEST</span>
+                </div>
+                <div className="goodsFormHeaderPrice">
+                  <div className="originalPrice">7,900원</div>
+                  <span className="saleRate">13%</span>
+                  <span className="salePrice">6,900원</span>
+                  <i className="fa-solid fa-share-nodes" />
+                </div>
+              </header>
+              <p className="goodsSummaryText">갈지 않은 원물 100%</p>
+              <div className="goodsFormItemDetail">
+                <p>
+                  <span className="itemDetailInfo">구매혜택</span> 100 포인트
+                  적립예정
+                </p>
+                <p>
+                  <span className="itemDetailInfo">배송 방법</span> 택배
+                </p>
+                <p>
+                  <span className="itemDetailInfo">배송비</span> 3,000원
+                  (30,000원 이상 무료 배송) | 도서산간 배송비 추가
+                </p>
+                <p>
+                  <span className="itemDetailInfo">배송 안내</span> 오후 1시까지
+                  주문 시 당일 출고 됩니다.
+                </p>
+              </div>
+              <div className="goodsFormItemQuantity">
+                <p className="goodsQuantity">수량 *</p>
+                <select className="goodsQuantitySelect">
+                  <option value={0}>0개</option>
+                  <option value={1}>1개</option>
+                  <option value={2}>2개</option>
+                  <option value={3}>3개</option>
+                  <option value={4}>4개</option>
+                  <option>직접입력</option>
+                </select>
+              </div>
+              <div className="goodsFormButton">
+                <button id="buyButton">구매하기</button>
+                <button>장바구니</button>
+                <button>
+                  <i className="fa-regular fa-heart" />
+                </button>
+              </div>
             </div>
-          </header>
-          <p className="goodsSummaryText">갈지 않은 원물 100%</p>
-          <div className="goodsFormItemDetail">
-            <p>
-              <span className="itemDetailInfo">구매혜택</span> 69 포인트
-              적립예정
-            </p>
-            <p>
-              <span className="itemDetailInfo">배송 방법</span> 택배
-            </p>
-            <p>
-              <span className="itemDetailInfo">배송비</span> 3,000원 (30,000원
-              이상 무료 배송) | 도서산간 배송비 추가
-            </p>
-            <p>
-              <span className="itemDetailInfo">배송 안내</span> 오후 1시까지
-              주문 시 당일 출고 됩니다.
-            </p>
-          </div>
-          <div className="goodsFormItemQuantity">
-            <p className="goodsQuantity">수량 *</p>
-            <select className="goodsQuantitySelect">
-              <option value={0}>0개</option>
-              <option value={1}>1개</option>
-              <option value={2}>2개</option>
-              <option value={3}>3개</option>
-              <option value={4}>4개</option>
-              <option>직접입력</option>
-            </select>
-          </div>
-          <div className="goodsFormButton">
-            <button id="buyButton">구매하기</button>
-            <button>장바구니</button>
-            <button>
-              <i className="fa-regular fa-heart" />
-            </button>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
