8038022bcf5926f6463e52635b016514df63c407
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-30 17:55:17 +0900

    Add: 변경

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cart from './pages/Cart/Cart';
-import Detail from './pages/Detail/Detail';
 import List from './pages/List/List';
 import Main from './pages/Main/Main';
 
@@ -11,7 +10,6 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/list" element={<List />} />
-        <Route path="/detail" element={<Detail />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
