005b4a0552f46742e1fb622387ad356cddcb2194
Haim Chi <ihaimichi@gmail.com>
2022-05-31 14:05:56 +0900

    Fixed: 라우터 수정

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Login from './pages/login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Cart from './pages/Cart/Cart';
-import Detail from './pages/Detail/Detail';
-import List from './pages/List/List';
 import Main from './pages/Main/Main';
-import SignUp from './pages/login/SignUp';
 
 const Router = () => {
   return (
@@ -19,10 +15,3 @@ const Router = () => {
 };
 
 export default Router;
-
-{
-  /* <Route path="/signup" element={<SignUp />} />
-<Route path="/list" element={<List />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/cart" element={<Cart />} /> */
-}
