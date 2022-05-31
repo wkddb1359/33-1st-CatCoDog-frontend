d054f2cc32e01d29639b8f51be8eab77a4897773
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-31 17:55:21 +0900

    Add: css 수정

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBottom from '../NavBottom/NavBottom';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Nav.scss';
 
 const Nav = () => {
