db250d640ead46ebde3de2624be69a47c44941ee
Yuntroll95 <yuntrollpark@gamail.com>
2022-06-01 17:06:39 +0900

    Add: router 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import Nav from '../../components/Nav/Nav';
 import RollingBannerWindow from '../../components/RollingBannerWindow/RollingBannerWindow';
 import RollingBanner from '../../components/RollingBanner/RollingBanner';
-import Footer from '../../components/Footer/Footer';
 import './Main.scss';
-import { Routes, Route } from 'react-router-dom';
-import SignUp from '../login/SignUp';
-import Cart from '../Cart/Cart';
 
 const Main = () => {
   return (
     <div className="main">
       <div className="mainContainer">
-        <Nav />
         <RollingBanner />
         <RollingBannerWindow />
         <div className="eventBannerDiv">
@@ -22,11 +16,6 @@ const Main = () => {
             alt="eventBanner"
           />
         </div>
-        <Routes>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
       </div>
     </div>
   );
