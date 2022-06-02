cf1068e4b2e740ea70c942cb776aca88781be93a
Yuntroll95 <yuntrollpark@gamail.com>
2022-06-02 16:33:30 +0900

    Fix: code 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -25,33 +25,4 @@ const Main = () => {
   );
 };
 
-export default Main; */
-
-import React from 'react';
-import RollingBannerWindow from '../../components/RollingBannerWindow/RollingBannerWindow';
-import RollingBanner from '../../components/RollingBanner/RollingBanner';
-
-import './Main.scss';
-import { Routes, Route } from 'react-router-dom';
-import SignUp from '../login/SignUp';
-import Cart from '../Cart/Cart';
-
-const Main = () => {
-  return (
-    <div className="main">
-      <div className="mainContainer">
-        <RollingBanner />
-        <RollingBannerWindow />
-        <div className="eventBannerDiv">
-          <img
-            src="/images/eventBanner.png"
-            className="eventBanner"
-            alt="eventBanner"
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export default Main;
